dc8b28cf66fef8fad70aa4f25a5eb01d095e1be3
DVerSan <davidverdeguersantolaria@gmail.com>
2023-12-13 23:04:21 +0100

    ui-theme.provider first approach - colors

---
+++ src/ui/ui-theme.provider.tsx
@@ -0,0 +1,15 @@
+export const colors = {
+  backgrounds: {
+    base: '#fffffe',
+    dark: '#eff0f3'
+  },
+  text: {
+    base: '#0d0d0d'
+  },
+  action: {
+    base: '#d9376e'
+  },
+  highlight: {
+    base: '#ff8e3c'
+  }
+}
