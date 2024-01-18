78e3fde7d572ed53f0cff8cb9ea7e1280b70a778
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-18 12:27:49 +0100

    Auth flow main layout props added

--- src/feature/auth/auth.navigator.tsx
+++ src/feature/auth/auth.navigator.tsx
@@ -7,13 +7,39 @@ import PasswordScreen from './login/password.screen'
 
 const AuthStack = createNativeStackNavigator()
 
+interface AuthLayoutProps {
+  paddingTop: number
+  mainSpacing: string
+  logoH: number
+  logoW: number
+}
+
 export default function AuthNavigator() {
+  const authLayoutProps: AuthLayoutProps = {
+    paddingTop: 8,
+    mainSpacing: 'xl',
+    logoH: 55,
+    logoW: 55
+  }
+
   return (
     <AuthStack.Navigator screenOptions={{ headerShown: false }}>
       <AuthStack.Screen name={'WelcomeScreen'} component={WelcomeScreen} />
-      <AuthStack.Screen name={'RegisterScreen'} component={RegisterScreen} />
-      <AuthStack.Screen name={'LoginScreen'} component={LoginScreen} />
-      <AuthStack.Screen name={'PasswordScreen'} component={PasswordScreen} />
+      <AuthStack.Screen
+        name={'RegisterScreen'}
+        component={RegisterScreen}
+        initialParams={{ authLayoutProps }}
+      />
+      <AuthStack.Screen
+        name={'LoginScreen'}
+        component={LoginScreen}
+        initialParams={{ authLayoutProps }}
+      />
+      <AuthStack.Screen
+        name={'PasswordScreen'}
+        component={PasswordScreen}
+        initialParams={{ authLayoutProps }}
+      />
     </AuthStack.Navigator>
   )
 }
