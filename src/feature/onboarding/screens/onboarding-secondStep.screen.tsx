1356e78c4ceaaf71360a47010fc7fcf92a1b59b5
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-26 14:08:24 +0100

    onboarding-secondStep.screen created - first approach

---
+++ src/feature/onboarding/screens/onboarding-secondStep.screen.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ScreenLayout from '../../../ui/layout/screen.layout'
+import Text from '../../../ui/components/text'
+import ScrollViewLayout from '../../../ui/layout/scrollview.layout'
+import VStackLayout from '../../../ui/layout/vstack.layout'
+
+export default function OnboardingSecondStepScreen({ route }: any) {
+  const { onboardingLayoutProps } = route.params
+
+  return (
+    <ScreenLayout>
+      <ScrollViewLayout fullHeight>
+        <VStackLayout
+          p={onboardingLayoutProps.padding}
+          pt={onboardingLayoutProps.paddingTop}
+          space={onboardingLayoutProps.mainSpacing}
+        >
+          <Text>{'ONBOARDING STEP2'}</Text>
+        </VStackLayout>
+      </ScrollViewLayout>
+    </ScreenLayout>
+  )
+}
