989a60e77c19a2be0f30e67b8a4e2a05430baa73
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-26 12:05:10 +0100

    OnboardingNavigator integrated into MainNavigator & Splash screen added

--- src/navigator/main.navigator.tsx
+++ src/navigator/main.navigator.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import AuthNavigator from '../feature/auth/auth.navigator'
+import { useAuth } from '../lib/auth/auth.context'
+import OnboardingNavigator from '../feature/onboarding/onboarding.navigator'
+import SplashScreen from 'react-native-splash-screen'
 
 const MainStack = createNativeStackNavigator()
 
 function MainNavigator() {
+  const { user, initializing } = useAuth()
+
+  if (initializing) {
+    return null
+  }
+
+  SplashScreen.hide()
+
   return (
     <MainStack.Navigator screenOptions={{ headerShown: false }}>
-      <MainStack.Screen name={'AuthNavigator'} component={AuthNavigator} />
+      {user ? (
+        <MainStack.Screen
+          name={'OnboardindNavigator'}
+          component={OnboardingNavigator}
+        />
+      ) : (
+        <MainStack.Screen name={'AuthNavigator'} component={AuthNavigator} />
+      )}
     </MainStack.Navigator>
   )
 }
