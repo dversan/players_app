0db20eca2744e7c79b0e1375ae82583df7be8660
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-26 10:54:15 +0100

    onboarding stack navigator created - first approach

---
+++ src/feature/onboarding/onboarding.navigator.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import OnboardingFirstStepScreen from './screens/onboarding-firstStep.screen'
+
+const AuthStack = createNativeStackNavigator()
+
+interface OnboardingLayoutProps {
+  padding: number
+  paddingTop: number
+  mainSpacing: string
+  logoH: number
+  logoW: number
+}
+
+export default function onboardingNavigator() {
+  const onboardingLayoutProps: OnboardingLayoutProps = {
+    padding: 24,
+    paddingTop: 8,
+    mainSpacing: 'xl',
+    logoH: 55,
+    logoW: 55
+  }
+
+  return (
+    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+      <AuthStack.Screen
+        name={'OnboardingFirstStep'}
+        component={OnboardingFirstStepScreen}
+        initialParams={{ onboardingLayoutProps }}
+      />
+    </AuthStack.Navigator>
+  )
+}
