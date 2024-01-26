bd755b62cece85340f7c8b96b1e2187149654e9e
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-26 16:12:44 +0100

    Accordion component created - first approach

---
+++ src/ui/components/accordion.tsx
@@ -0,0 +1,24 @@
+import { Accordion as GSAccordion } from '@gluestack-ui/themed'
+import React, { PropsWithChildren } from 'react'
+import { ViewProps } from 'react-native'
+
+interface GSAccordionProps extends PropsWithChildren<ViewProps> {
+  size?: 'sm' | 'md' | 'lg' | 'xl'
+  variant?: 'filled' | 'unfilled'
+  type?: 'single' | 'multiple'
+  disabled?: boolean
+}
+
+export default function Accordion(props: GSAccordionProps) {
+  return (
+    <GSAccordion
+      size={props.size}
+      variant={props.variant}
+      type={props.type}
+      isCollapsible={props.collapsable}
+      isDisabled={props.disabled}
+    >
+      {props.children}
+    </GSAccordion>
+  )
+}
