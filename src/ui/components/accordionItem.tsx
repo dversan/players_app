b9729e1f4c2f50e3f9f79b40e9d6e8dbac2ec0f3
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-27 09:13:27 +0100

    Accordion item created - first approach

---
+++ src/ui/components/accordionItem.tsx
@@ -0,0 +1,47 @@
+import {
+  AccordionItem as GSAccordionItem,
+  AccordionContent,
+  AccordionHeader,
+  AccordionTitleText,
+  AccordionTrigger
+} from '@gluestack-ui/themed'
+import React, { PropsWithChildren } from 'react'
+import { ViewProps } from 'react-native'
+
+interface GSAccordionProps extends PropsWithChildren<ViewProps> {
+  headerTitle: string
+  itemValue: string
+  isDisabled?: boolean
+  borderRadius?: number
+  margin?:
+    | {
+        mt?: 8 | 16 | 24
+        mb?: 8 | 16 | 24
+        ml?: 8 | 16 | 24
+        mr?: 8 | 16 | 24
+      }
+    | number
+}
+
+export default function AccordionItem(props: GSAccordionProps) {
+  return (
+    <GSAccordionItem
+      value={props.itemValue}
+      backgroundColor={'red'}
+      borderRadius={8}
+      shadowColor={'transparent'}
+      sx={props.margin}
+    >
+      <AccordionHeader>
+        <AccordionTrigger>
+          {() => {
+            return <AccordionTitleText>{props.headerTitle}</AccordionTitleText>
+          }}
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent borderTopWidth={1} borderColor='black' paddingTop={16}>
+        {props.children}
+      </AccordionContent>
+    </GSAccordionItem>
+  )
+}
