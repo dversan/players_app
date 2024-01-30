5d8e5fcb59f306d804af21983462f7d6853114e4
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-30 11:51:43 +0100

    Input component updated with conditional styles

--- src/ui/components/input.tsx
+++ src/ui/components/input.tsx
@@ -13,17 +13,15 @@ interface GSInputProps extends PropsWithChildren<TextInputProps> {
   label?: string
   error?: string
   type?: string
-  textColor?: string
   flex?: number
+  formType?: 'register' | 'onboarding'
 }
 
 export default function Input(props: GSInputProps) {
   return (
     <FormControl flex={props.flex} isInvalid={props.error !== undefined}>
       {props.label && (
-        <FormControlLabelText
-          color={colors.backgroundLight500}
-        >
+        <FormControlLabelText color={colors.backgroundLight500}>
           {props.label}
         </FormControlLabelText>
       )}
@@ -32,24 +30,19 @@ export default function Input(props: GSInputProps) {
         isInvalid={props.error !== undefined}
         borderRadius={4}
         size={'xl'}
-        sx={{
-          ':focus': {
-            bg: colors.backgroundDark300,
-            borderColor: colors.primary300,
-            borderWidth: 2
-          },
-          ':invalid': {
-            borderColor: 'red'
-          }
-        }}
+        sx={
+          props.formType === 'onboarding'
+            ? inputStyle.onboarding
+            : inputStyle.register
+        }
       >
         <InputField
           value={props.value}
           type={props.type}
           onChangeText={props.onChangeText}
           selectionColor={'white'}
-          color={props.textColor || 'white'}
-          style={{ ...props.style }}
+          color={inputStyle.onboarding.text.color || 'white'}
+          style={{ ...(props.style, inputStyle.onboarding.text) }}
         />
       </GSInput>
       {props.error && (
@@ -57,4 +50,30 @@ export default function Input(props: GSInputProps) {
       )}
     </FormControl>
   )
+}
+
+export const inputStyle = {
+  register: {
+    ':focus': {
+      backgroundColor: colors.backgroundDark300,
+      borderColor: colors.primary300,
+      borderWidth: 2
+    },
+    ':invalid': {
+      borderColor: 'red'
+    }
+  },
+  onboarding: {
+    ':focus': {
+      backgroundColor: colors.backgroundDark800,
+      borderColor: colors.primary300,
+      borderWidth: 3
+    },
+    text: {
+      textAlign: 'center',
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white'
+    }
+  }
 }
