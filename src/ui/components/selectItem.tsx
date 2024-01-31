5281c7a889820b112fcae75e4220bd9d054e5bcd
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-31 10:09:15 +0100

    SelectItem component created

---
+++ src/ui/components/selectItem.tsx
@@ -0,0 +1,13 @@
+import { SelectItem as GSSelectItem } from '@gluestack-ui/themed'
+import { PropsWithChildren } from 'react'
+import { PressableProps } from 'react-native'
+
+interface SelectItemProps extends PropsWithChildren<PressableProps> {
+  label: string
+  value: any
+  isDisabled?: boolean
+}
+
+export default function SelectItem(props: SelectItemProps) {
+  return <GSSelectItem>{props.children}</GSSelectItem>
+}
