e2d6abb3fcc84dbf7bea088d3f6b53df1496a43b
DVerSan <davidverdeguersantolaria@gmail.com>
2024-02-14 13:00:18 +0100

    SafeaAreaProvider added to the custom render for testing

--- __tests__/__utils__/test-utils.tsx
+++ __tests__/__utils__/test-utils.tsx
@@ -1,23 +1,25 @@
 // https://testing-library.com/docs/react-testing-library/setup#custom-render
 import React, { ReactElement } from 'react'
-import { render } from '@testing-library/react-native'
+import { render as rtlRender } from '@testing-library/react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { GluestackUIStyledProvider } from '@gluestack-ui/themed'
 import gluestackCustomUIConfig from '../../src/ui/ui-theme.provider'
 
 const GluestackUIProviderWrapper = ({ children }: any) => (
-  <GluestackUIStyledProvider
-    initialWindowMetrics={{
+  <SafeAreaProvider
+    initialMetrics={{
       frame: { x: 0, y: 0, width: 0, height: 0 },
       insets: { top: 0, left: 0, right: 0, bottom: 0 }
     }}
-    config={gluestackCustomUIConfig}
   >
-    {children}
-  </GluestackUIStyledProvider>
+    <GluestackUIStyledProvider config={gluestackCustomUIConfig}>
+      {children}
+    </GluestackUIStyledProvider>
+  </SafeAreaProvider>
 )
 
 const customRender = (ui: ReactElement, options?: Omit<any, 'wrapper'>) =>
-  render(ui, { wrapper: GluestackUIProviderWrapper, ...options })
+  rtlRender(ui, { wrapper: GluestackUIProviderWrapper, ...options })
 
 export * from '@testing-library/react-native'
 export { customRender as render }
