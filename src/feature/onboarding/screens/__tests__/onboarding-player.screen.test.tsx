d02667988d390487bab012dc95a5b1fbde6b08fa
DVerSan <davidverdeguersantolaria@gmail.com>
2024-02-14 12:55:49 +0100

    onboarding-player.screen.test created - first approach

---
+++ src/feature/onboarding/screens/__tests__/onboarding-player.screen.test.tsx
@@ -0,0 +1,49 @@
+import { describe, afterEach, it, expect, jest } from '@jest/globals'
+import sinon from 'sinon'
+import { screen, waitFor } from '@testing-library/react-native'
+import { render } from '../../../../../__tests__/__utils__/test-utils'
+import * as Auth from '../../../../lib/auth/auth.context'
+import { User } from '../../../../lib/data/models'
+import OnboardingPlayerScreen from '../../../../feature/onboarding/screens/onboarding-player.screen'
+
+describe('OnboardingPlayerScreen test', () => {
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  it('Should show the onboarding if user is registered and the onboarding is unfinished.', async () => {
+    sinon.stub(Auth, 'useAuth').returns({
+      ...Auth.useAuth(),
+      user: {
+        playerData: { playerNickname: 'New Player' }
+      } as User
+    })
+
+    // Mock route and navigation props
+    const mockRoute = {
+      params: {
+        onboardingLayoutProps: {
+          padding: 24,
+          paddingTop: 8,
+          mainSpacing: 'xl',
+          logoH: 55,
+          logoW: 55
+        }
+      }
+    }
+    const mockNavigation = {
+      navigate: jest.fn()
+    }
+
+    await waitFor(() => {
+      // Pass the mocked props to your component when rendering
+      render(
+        <OnboardingPlayerScreen route={mockRoute} navigation={mockNavigation} />
+      )
+      // screen.debug()
+
+      expect(screen.getByText('ONBOARDINGSCREEN.TITLE')).toBeTruthy()
+      // ace with actual expectation
+    })
+  })
+})
