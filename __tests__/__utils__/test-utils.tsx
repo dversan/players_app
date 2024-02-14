7210216feaa18e13352b4c0d84e47061c37eab88
DVerSan <davidverdeguersantolaria@gmail.com>
2024-02-14 11:04:58 +0100

    Set custom render for testing

--- __tests__/__utils__/test-utils.tsx
+++ __tests__/__utils__/test-utils.tsx
@@ -1,21 +1,23 @@
 // https://testing-library.com/docs/react-testing-library/setup#custom-render
 import React, { ReactElement } from 'react'
 import { render } from '@testing-library/react-native'
-import { NativeBaseProvider } from 'native-base'
+import { GluestackUIStyledProvider } from '@gluestack-ui/themed'
+import gluestackCustomUIConfig from '../../src/ui/ui-theme.provider'
 
-const NativeBaseProviderWrapper = ({ children }: any) => (
-  <NativeBaseProvider
+const GluestackUIProviderWrapper = ({ children }: any) => (
+  <GluestackUIStyledProvider
     initialWindowMetrics={{
       frame: { x: 0, y: 0, width: 0, height: 0 },
       insets: { top: 0, left: 0, right: 0, bottom: 0 }
     }}
-    config={{ suppressColorAccessibilityWarning: true }}>
+    config={gluestackCustomUIConfig}
+  >
     {children}
-  </NativeBaseProvider>
+  </GluestackUIStyledProvider>
 )
 
 const customRender = (ui: ReactElement, options?: Omit<any, 'wrapper'>) =>
-  render(ui, { wrapper: NativeBaseProviderWrapper, ...options })
+  render(ui, { wrapper: GluestackUIProviderWrapper, ...options })
 
 export * from '@testing-library/react-native'
 export { customRender as render }
