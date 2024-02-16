5c5c8cba9183d564e6f83e304ef9b969a48146eb
DVerSan <davidverdeguersantolaria@gmail.com>
2024-02-16 11:54:06 +0100

    babel.config.js & metro.config.js updated

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,38 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
-module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@react-native/babel-preset',
-    '@babel/preset-typescript'
-  ],
-  plugins: []
+const tsconfig = require('./tsconfig.json')
+const rawAlias = tsconfig.compilerOptions.paths
+const alias = {}
+
+for (const x in rawAlias) {
+  alias[x.replace('/*', '')] = rawAlias[x].map(p => p.replace('/*', ''))
+}
+
+module.exports = function (api) {
+  api.cache(true)
+
+  return {
+    presets: [
+      'module:metro-react-native-babel-preset',
+      '@babel/preset-typescript'
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.js',
+            '.ts',
+            '.tsx',
+            '.json',
+            '.svg'
+          ],
+          alias
+        }
+      ]
+    ]
+  }
 }
