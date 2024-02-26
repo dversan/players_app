5c17e80ec69bfde51053972ce932739dc0c2f597
DVerSan <davidverdeguersantolaria@gmail.com>
2024-02-26 13:03:43 +0100

    VStackLayout props updated

--- src/ui/layout/vstack.layout.tsx
+++ src/ui/layout/vstack.layout.tsx
@@ -1,6 +1,11 @@
 import { VStack as GSVStack } from '@gluestack-ui/themed'
 import { ViewProps } from 'react-native'
 
-export default function VStackLayout(props: ViewProps) {
+interface GSVStackProps extends ViewProps {
+  alignItems?: 'left' | 'center' | 'right'
+  space?: string
+}
+
+export default function VStackLayout(props: GSVStackProps) {
   return <GSVStack {...props}>{props.children}</GSVStack>
 }
