f477c289f5e7d07d53300a153fcdd6624217b15d
DVerSan <davidverdeguersantolaria@gmail.com>
2024-02-26 13:09:08 +0100

    Button-card created

---
+++ src/ui/components/button-card.tsx
@@ -0,0 +1,36 @@
+import React, { PropsWithChildren } from 'react'
+import { Box, Center, Image, Pressable } from '@gluestack-ui/themed'
+import Text from './text'
+import HStackLayout from '../layout/hstack.layout'
+import VStackLayout from '../layout/vstack.layout'
+import { ViewProps } from 'react-native'
+import { customColors as colors } from '../../ui/ui-theme.provider'
+
+interface CardProps extends PropsWithChildren<ViewProps> {
+  title: string
+  text: string
+  imageUrl: string
+  onPress?: () => void
+}
+
+export default function ButtonCard(props: CardProps) {
+  return (
+    <Pressable onPress={props.onPress}>
+      <Box {...props} p={8} bg={colors.backgroundDark800} borderRadius={8}>
+        <HStackLayout space={'md'}>
+          <Center>
+            <Image source={props.imageUrl} alt={'player option card img'} />
+          </Center>
+          <VStackLayout flex={1} space={'xs'}>
+            <Text size={'lg'} bold>
+              {props.title}
+            </Text>
+            <Text lineHeight={20} color={colors.lightSecondaryText300}>
+              {props.text}
+            </Text>
+          </VStackLayout>
+        </HStackLayout>
+      </Box>
+    </Pressable>
+  )
+}
