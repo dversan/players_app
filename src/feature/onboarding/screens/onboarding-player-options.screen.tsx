df247faa47aede0faa7c80fc10da7cc861787d13
DVerSan <davidverdeguersantolaria@gmail.com>
2024-02-28 09:45:23 +0100

    onboardingPlayerOptions refactored with button cards

--- src/feature/onboarding/screens/onboarding-player-options.screen.tsx
+++ src/feature/onboarding/screens/onboarding-player-options.screen.tsx
@@ -1,21 +1,56 @@
 import React from 'react'
-import ScreenLayout from '../../../ui/layout/screen.layout'
-import Text from '../../../ui/components/text'
-import ScrollViewLayout from '../../../ui/layout/scrollview.layout'
-import VStackLayout from '../../../ui/layout/vstack.layout'
+import ScreenLayout from '@ui/layout/screen.layout'
+import ScrollViewLayout from '@ui/layout/scrollview.layout'
+import VStackLayout from '@ui/layout/vstack.layout'
+import ButtonCard from '@ui/components/button-card'
+import { t } from 'i18next'
+import { Center, Image } from '@gluestack-ui/themed'
+import Text from '@ui/components/text'
+import { useAuth } from '@lib/auth/auth.context'
 
 export default function OnboardingPlayerOptionsScreen({ route }: any) {
   const { onboardingLayoutProps } = route.params
+  const { user } = useAuth()
 
   return (
     <ScreenLayout>
       <ScrollViewLayout fullHeight>
         <VStackLayout
-          p={onboardingLayoutProps.padding}
+          p={16}
           pt={onboardingLayoutProps.paddingTop}
-          space={onboardingLayoutProps.mainSpacing}
+          space={'3xl'}
         >
-          <Text>{'ONBOARDING PLAYER OPTIONS'}</Text>
+          <Center>
+            <Image
+              height={onboardingLayoutProps.logoH}
+              width={onboardingLayoutProps.logoW}
+              source={require('../../../ui/images/players_logo.jpeg')}
+              alt={'players logo'}
+            />
+          </Center>
+          <VStackLayout space={'lg'} alignItems={'center'}>
+            <Text size={'3xl'} bold>
+              {t('onboardingPlayerOptions.title', { name: user.name })}
+            </Text>
+            <Text>{t('onboardingPlayerOptions.description')}</Text>
+          </VStackLayout>
+          <VStackLayout flex={1} space={'lg'}>
+            <ButtonCard
+              title={t('onboardingPlayerOptions.goToProfile')}
+              text={t('onboardingPlayerOptions.goToProfileDescription')}
+              imageUrl={require('../../../ui/images/goToProfile.png')}
+            />
+            <ButtonCard
+              title={t('onboardingPlayerOptions.joinClub')}
+              text={t('onboardingPlayerOptions.joinClubDescription')}
+              imageUrl={require('../../../ui/images/joinClub.png')}
+            />
+            <ButtonCard
+              title={t('onboardingPlayerOptions.createNewClub')}
+              text={t('onboardingPlayerOptions.createNewClubDescription')}
+              imageUrl={require('../../../ui/images/createNewClub.png')}
+            />
+          </VStackLayout>
         </VStackLayout>
       </ScrollViewLayout>
     </ScreenLayout>
