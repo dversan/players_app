c3482c6cfb1b2f971913023ce717c36e8419a65d
DVerSan <davidverdeguersantolaria@gmail.com>
2024-03-13 16:03:28 +0100

    onboarding-parameters-form refactored

--- src/ui/components/onboarding-parameters-form.tsx
+++ src/ui/components/onboarding-parameters-form.tsx
@@ -1,38 +1,80 @@
 import HStackLayout from '@ui/layout/hstack.layout'
 import Input from '@ui/components/input'
 import { t } from 'i18next'
-import { PlayerData } from '@lib/data/models'
+import {
+  OnboardingSteps,
+  ParametersFormData,
+  PlayerData
+} from '@lib/data/models'
 import VStackLayout from '@ui/layout/vstack.layout'
-import React, { useEffect } from 'react'
+import React, { useRef, useState } from 'react'
 import Text from '@ui/components/text'
-import { ValidationFields } from '@lib/data/helpers'
+import {
+  OnboardingParametersStepValidation,
+  ValidationFields
+} from '@lib/data/helpers'
 import { calculateFitnessParameter } from '@lib/data/calculators'
+import Button from '@ui/components/button'
 
 interface OnboardingFormParametersProps {
-  onSetFormData: (
-    fieldName: keyof PlayerData,
-    fieldValue: string | number | Date
-  ) => void
-  validation: { [key: keyof ValidationFields]: string }
-  parametersData: PlayerData
+  onSetFormData: (formData, step: OnboardingSteps, stepToOpen: null) => void
+  calculationInputs: PlayerData
 }
 
 const OnboardingParametersForm = ({
   onSetFormData,
-  validation,
-  parametersData
+  calculationInputs
 }: OnboardingFormParametersProps) => {
-  const fitnessValue = calculateFitnessParameter(
-    parametersData.playerHeight,
-    parametersData.playerWeight,
-    parametersData.birthday,
-    parametersData.gamesPerYearIndex,
-    parametersData.competitionGamesIndex
+  useState<boolean>(false)
+  const [errors, setErrors] = useState<{
+    [key: keyof ValidationFields]: string
+  }>({})
+
+  const parametersFormDataRef = useRef<ParametersFormData>({
+    attack: 0,
+    defense: 0,
+    fitness: 0,
+    goal: 0,
+    pass: 0,
+    teamWork: 0
+  })
+
+  const fitnessParameterValue = calculateFitnessParameter(
+    calculationInputs.playerHeight,
+    calculationInputs.playerWeight,
+    calculationInputs.birthday,
+    calculationInputs.gamesPerYearIndex,
+    calculationInputs.competitionGamesIndex
   )
 
-  useEffect(() => {
-    onSetFormData('fitness', fitnessValue)
-  }, [parametersData.attack])
+  const setFormDataRef = (fieldName, fieldValue) => {
+    parametersFormDataRef.current = {
+      ...parametersFormDataRef.current,
+      fitness: fitnessParameterValue,
+      [fieldName]: fieldValue
+    }
+  }
+
+  const confirmStepButtonHandler = () => {
+    if (
+      OnboardingParametersStepValidation(parametersFormDataRef.current)[
+        `${OnboardingSteps.PARAMETERS}ValidationOk`
+      ]
+    ) {
+      setErrors({})
+      onSetFormData(
+        parametersFormDataRef.current,
+        OnboardingSteps.PARAMETERS,
+        null
+      )
+    } else {
+      setErrors(
+        OnboardingParametersStepValidation(parametersFormDataRef.current)[
+          `${OnboardingSteps.PARAMETERS}ValidationErrors`
+        ]
+      )
+    }
+  }
 
   return (
     <VStackLayout space={'lg'}>
@@ -42,22 +84,22 @@ const OnboardingParametersForm = ({
           label={t('onboardingScreen.attack')}
           flex={1}
           formType={'onboarding'}
-          onChangeText={value => onSetFormData('attack', Number(value))}
-          error={validation.attack}
+          onChangeText={value => setFormDataRef('attack', Number(value))}
+          error={errors.attack}
         />
         <Input
           label={t('onboardingScreen.defense')}
           flex={1}
           formType={'onboarding'}
-          onChangeText={value => onSetFormData('defense', Number(value))}
-          error={validation.defense}
+          onChangeText={value => setFormDataRef('defense', Number(value))}
+          error={errors.defense}
         />
         <Input
           label={t('onboardingScreen.fitness')}
           flex={1}
           formType={'onboarding'}
-          value={fitnessValue.toString()}
-          error={validation.fitness}
+          value={fitnessParameterValue.toString()}
+          error={errors.fitness}
           isReadOnly
         />
       </HStackLayout>
@@ -66,27 +108,36 @@ const OnboardingParametersForm = ({
           label={t('onboardingScreen.goal')}
           flex={1}
           formType={'onboarding'}
-          onChangeText={value => onSetFormData('goal', Number(value))}
-          error={validation.goal}
+          onChangeText={value => setFormDataRef('goal', Number(value))}
+          error={errors.goal}
         />
         <Input
           label={t('onboardingScreen.pass')}
           flex={1}
           formType={'onboarding'}
-          onChangeText={value => onSetFormData('pass', Number(value))}
-          error={validation.pass}
+          onChangeText={value => setFormDataRef('pass', Number(value))}
+          error={errors.pass}
         />
         <Input
           label={t('onboardingScreen.teamWork')}
           flex={1}
           formType={'onboarding'}
-          onChangeText={value => onSetFormData('teamWork', Number(value))}
-          error={validation.teamWork}
+          onChangeText={value => setFormDataRef('teamWork', Number(value))}
+          error={errors.teamWork}
         />
       </HStackLayout>
-      {Object.keys(validation).length > 0 && (
-        <Text color={'red'}>{Object.values(validation)[0]}</Text>
+      {Object.keys(errors).length > 0 && (
+        <Text color={'red'}>{Object.values(errors)[0]}</Text>
       )}
+      <Button
+        testID={'parametersStepButton'}
+        size={'lg'}
+        mt={32}
+        alignSelf={'center'}
+        onPress={confirmStepButtonHandler}
+      >
+        {t('onboardingScreen.confirmStep')}
+      </Button>
     </VStackLayout>
   )
 }
