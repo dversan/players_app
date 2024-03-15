32645e2d7f07d7142bc59555b8756224d5370782
DVerSan <davidverdeguersantolaria@gmail.com>
2024-03-15 22:58:39 +0100

    tab.navigator done

--- src/feature/tab.navigator.tsx
+++ src/feature/tab.navigator.tsx
@@ -1,35 +1,59 @@
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from '../feature/contents/screens/home-screen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ContentsNavigator from './contents/contents.navigator'
+import NotificationsNavigator from './notifications/notifications.navigator'
+import ProfileNavigator from './profile/profile.navigator'
+import { customColors as colors } from '@ui/ui-theme.provider'
+import { BellIcon, HomeIcon, UserIcon } from 'lucide-react-native'
 
-const Onboarding = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
-interface OnboardingLayoutProps {
-  padding: number
-  paddingTop: number
-  mainSpacing: string
-  logoH: number
-  logoW: number
-}
-
-const OnboardingNavigator = () => {
-  const onboardingLayoutProps: OnboardingLayoutProps = {
-    padding: 24,
-    paddingTop: 8,
-    mainSpacing: 'xl',
-    logoH: 55,
-    logoW: 55
-  }
-
+const TabNavigator = () => {
   return (
-    <Onboarding.Navigator screenOptions={{ headerShown: false }}>
-      <Onboarding.Screen
-        name={'HomeScreen'}
-        component={HomeScreen}
-        initialParams={{ onboardingLayoutProps }}
+    <Tab.Navigator
+      sceneContainerStyle={{ backgroundColor: 'red' }}
+      backBehavior={'none'}
+      screenOptions={() => {
+        return {
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: colors.backgroundDark500,
+            paddingVertical: 16
+          },
+          tabBarLabelStyle: { fontSize: 12, marginTop: 8 },
+          tabBarActiveTintColor: colors.primary500
+        }
+      }}
+    >
+      <Tab.Screen
+        name={'Home'}
+        component={ContentsNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <HomeIcon name='home' color={color} size={size} />
+          )
+        }}
       />
-    </Onboarding.Navigator>
+      <Tab.Screen
+        name={'Notifications'}
+        component={NotificationsNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <BellIcon name='home' color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name={'Profile'}
+        component={ProfileNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <UserIcon name='home' color={color} size={size} />
+          )
+        }}
+      />
+    </Tab.Navigator>
   )
 }
 
-export default OnboardingNavigator
+export default TabNavigator
