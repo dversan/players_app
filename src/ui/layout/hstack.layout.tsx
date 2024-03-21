d0d8e497e1c71a44c30d43f88b835c8a45db38fa
DVerSan <davidverdeguersantolaria@gmail.com>
2024-03-21 18:43:50 +0100

    props added to HStackLayout

--- src/ui/layout/hstack.layout.tsx
+++ src/ui/layout/hstack.layout.tsx
@@ -1,7 +1,13 @@
 import { HStack as GSHStack } from '@gluestack-ui/themed'
 import { ViewProps } from 'react-native'
 
-const HStackLayout = (props: ViewProps) => {
+interface GSHStackProps extends ViewProps {
+  alignSelf?: 'left' | 'center' | 'right'
+  justifyContent?: 'top' | 'center' | 'bottom'
+  space?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl'
+}
+
+const HStackLayout = (props: GSHStackProps) => {
   return <GSHStack {...props}>{props.children}</GSHStack>
 }
 
